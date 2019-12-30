aacb723fd21749ff04c8bfaefbc5bca36ffbf58f
David <mwikyakimolo@gmail.com>
2019-12-30 22:47:35 +0300

    added Vendor class

---
+++ lms/vendor.py
@@ -0,0 +1,22 @@
+# Vendor class
+class Vendor:
+    """ this is the Vendors class """
+    def __init__(self, book_name, book_price, book_type):
+        """ this are the attributes of the Vendor class """
+        self. book_name = book_name
+        self.book_price = book_price
+        self.book_type = book_type
+    
+    def search(self):
+        """ This searches for books """
+        pass
+
+    def supply_book(self):
+        """ This shows the books that need to be supplied as requested by patron"""
+        pass
+
+    def payment_details(self):
+        """ this shows the payment details where the funds will go """
+        pass
+    
+
