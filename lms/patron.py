f508e664fb7bb0b08527adbcfa4a6386c2768b7f
David <mwikyakimolo@gmail.com>
2019-12-30 22:10:14 +0300

    added Patron class

---
+++ lms/patron.py
@@ -0,0 +1,24 @@
+class Patron:
+    """ this is the Patron class """
+    def __init__(self, name, email, patron_id):
+        """ this are the patron class attributes """
+        self.name = name
+        self.email = email
+        self. patron_id = patron_id
+
+    def search(self):
+        """ this will enable the patron search a book """
+        pass
+
+    def request(self):
+        """ this enables the patron to see book requests """
+        pass
+    
+    def pay_fine(self):
+        """ this will make the patron be able to pay fines """
+        pass
+
+
+
+
+    
