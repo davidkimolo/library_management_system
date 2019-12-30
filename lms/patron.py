b349c33f80140d82dba625b0e5a0c4ea8b377b94
David <mwikyakimolo@gmail.com>
2019-12-30 22:33:46 +0300

    added Patron_record child class

--- lms/patron.py
+++ lms/patron.py
@@ -18,7 +18,17 @@ class Patron:
         """ this will make the patron be able to pay fines """
         pass
 
+class Patron_record(Patron):
+    """ this is a child class of the parent Patron """
+    def __init__(self, name, email, patron_id, date_of_membership, no_of_books_issued, max_no_limit, phone_no, fines_owed):
+        super().__init__(name, email, patron_id)
+        """ this are thr attributes of the child class Pareon_record """
+        self.patron_id = patron_id
+        self.name  = name
+        self.email = email
+        self.date_of_membership = date_of_membership
+        self.number_of_books_issued = no_of_books_issued
+        self.maximum_number_limit = max_no_limit
+        self.phone_number = phone_no
+        self.fines_owed = fines_owed
 
-
-
-    
