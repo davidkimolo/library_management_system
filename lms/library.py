1b78f27c23d74e49be8e6ae51c845b6f56efdf67
David <mwikyakimolo@gmail.com>
2019-12-30 15:08:43 +0300

    Parent Library class

---
+++ lms/library.py
@@ -0,0 +1,6 @@
+class Library:
+    """ This is the parent Library class """
+    def __init__(self, location, librarian_id):
+        """ this are the attributes of the Library class """
+        self.location = location
+        self.librarian_id = librarian_id
